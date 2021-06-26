241709209ae47e7b874235bb9abe72962fd4f8dd
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-06-26 14:39:35 +0300

    fix

---
+++ public/js/js/import/modules.js
@@ -0,0 +1,8 @@
+import "%modules%/header/header";
+import "%modules%/faq-list/faq-list";
+import "%modules%/comment-list-small/comment-list-small";
+import "%modules%/footer/footer";
+import "%modules%/service-form/service-form";
+import "%modules%/article-list/article-list";
+
+import "%modules%/contact/contact-form/contact-form";
